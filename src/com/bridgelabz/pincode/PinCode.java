cd40cb18b65f45cab97a3a9304cf5d28dfc98218
shruthishruu05 <shruthi.m@perfios.com>
2021-09-07 19:39:52 +0530

    ADDED : Method to have space between pin code

--- src/com/bridgelabz/pincode/PinCode.java
+++ src/com/bridgelabz/pincode/PinCode.java
@@ -5,16 +5,16 @@ import java.util.regex.Matcher;
 public class PinCode
 {
 	public static boolean pinCodeChecker(String pincode) {
-		Pattern pattern = Pattern.compile("^[0-9]{6}$");
+		Pattern pattern = Pattern.compile("(^[0-9]{3})([\s]?)([0-9]{3}$)");
 		Matcher matcher = pattern.matcher(pincode);
 		return matcher.matches();
 	}
 
 	public static void main(String[] args) {
 		
-		System.out.println("PINCode Evaluator \nenter a pin code");
+		System.out.println("Enter a pin code");
 		Scanner sc = new Scanner(System.in);
-		String pincode = sc.next();
+		String pincode = sc.nextLine();
 		
 		if(pinCodeChecker(pincode))System.out.println("It is a valid pincode");
 		else System.out.println("It is an invalid pincode");
