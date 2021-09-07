76430f2fd8ec050f4cae036628013aa96e10a3ce
shruthishruu05 <shruthi.m@perfios.com>
2021-09-07 19:16:59 +0530

    ADDED : Method to restrict the pin code to take alphabet and special character

--- src/com/bridgelabz/pincode/PinCode.java
+++ src/com/bridgelabz/pincode/PinCode.java
@@ -4,21 +4,20 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 public class PinCode
 {
-		public static boolean pinCodeCheck(String pincode) 
-		{
-			Pattern pattern = Pattern.compile("[0-9]{6}");
-			Matcher matcher = pattern.matcher(pincode);
-			return matcher.matches();
-		}
+	public static boolean pinCodeChecker(String pincode) {
+		Pattern pattern = Pattern.compile("^[0-9]{6}");
+		Matcher matcher = pattern.matcher(pincode);
+		return matcher.matches();
+	}
 
-		public static void main(String[] args) {
-			
-			System.out.println("PINCode Evaluator \nenter a pin code");
-			Scanner sc = new Scanner(System.in);
-			String pincode = sc.next();
-			
-			if(pinCodeCheck(pincode))System.out.println("It is a valid pincode");
-			else System.out.println("It is an invalid pincode");
-		}
+	public static void main(String[] args) {
+		
+		System.out.println("Enter a pin code");
+		Scanner sc = new Scanner(System.in);
+		String pincode = sc.next();
+		
+		if(pinCodeChecker(pincode))System.out.println("It is a valid pincode");
+		else System.out.println("It is an invalid pincode");
 
 	}
+}
